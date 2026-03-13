83a7339068521d842ded91704b98ee48cc2db69c
Ho-jin98 <hohoj98@naver.com>
2026-03-13 14:02:30 +0900

    feat: Initial commit

---
+++ src/main/java/com/example/teamprojectpaymentsystem/TeamProjectPaymentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.teamprojectpaymentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamProjectPaymentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamProjectPaymentSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/teamprojectpaymentsystem/TeamProjectPaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teamprojectpaymentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProjectPaymentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
